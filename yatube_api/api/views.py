7598ac0c9ae42b0e159e7782bc771c39b87f3ce7
Buzhak <Byzhak@ya.ru>
2022-10-27 19:11:08 +0300

    fix

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404
 
-from rest_framework import viewsets, mixins, exceptions, permissions
+from rest_framework import viewsets, mixins, permissions
 from rest_framework.pagination import LimitOffsetPagination
 from rest_framework import filters
 
